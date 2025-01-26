4905561020a3e48dab14b983a743cd06cb3a293f
musk96 <musk96@hotmail.fr>
2025-01-26 22:39:40 +0100

    added allowed hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1j(qh9#=f(i7m%93*d+pca5n!%d*iah84=ub*z+3sguiuz)yff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
